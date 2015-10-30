b4822c317230a454d947b24922eeeaccbc77fae4
Ant Cosentino <antony@mycosentino.com>
2015-10-30 14:38:26 +0200

    updated and added test cases

--- test/index.js
+++ test/index.js
@@ -5,9 +5,24 @@ var expect = require('chai').expect;
 var chunk = require('../index');
 
 describe('chunk', function() {
-  it('should split an array into chunks of a given length', function() {
+  it('should work for even chunkings', function() {
     var input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     var expected = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]];
-    expect(chunk.call(input, 2)).to.deep.equal(expected);
+    expect(chunk(input, 2)).to.deep.equal(expected);
+  });
+  it('should work for non-even chunkings', function() {
+    var input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var expected = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]];
+    expect(chunk(input, 3)).to.deep.equal(expected);
+  });
+  it('shouldn\'t freak out with zero-length arrays', function() {
+    var input = [];
+    var expected = [];
+    expect(chunk(input, 2)).to.deep.equal(expected);
+  });
+  it('shoudln\'t freak out for negative chunk lengths', function() {
+    var input = [];
+    var expected = [];
+    expect(chunk(input, -1)).to.deep.equal(expected);
   });
 });
