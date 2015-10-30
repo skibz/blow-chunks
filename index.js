95ce299a09ea8f71daa8fb88de9d2b056778b180
Ant Cosentino <antony@mycosentino.com>
2015-10-30 14:39:19 +0200

    removed unneeded context argument

--- index.js
+++ index.js
@@ -4,5 +4,5 @@
 module.exports = function(array, size) {
   return [].concat.apply([], array.map(function(element, index) {
     return index % size ? [] : [array.slice(index, index + size)];
-  }, array));
+  }));
 };
