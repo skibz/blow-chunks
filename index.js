dfd90cf8a4931de36bc645fc696f4bb33ed17b15
Ant Cosentino <antony@mycosentino.com>
2015-10-30 14:38:19 +0200

    added array parameter to fix operating on

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 'use strict';
 
-module.exports = function(size) {
-  return [].concat.apply([], this.map(function(element, index) {
-    return index % size ? [] : [this.slice(index, index + size)];
-  }, this));
+module.exports = function(array, size) {
+  return [].concat.apply([], array.map(function(element, index) {
+    return index % size ? [] : [array.slice(index, index + size)];
+  }, array));
 };
